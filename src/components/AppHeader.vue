e6d15a406bb7ab16761db12fdf8732dac702e785
fabiodelli <ntqdel@hotmail.com>
2023-06-16 15:51:10 +0200

    add scss modify views

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -5,7 +5,7 @@ export default {
 </script>
 <template>
     <header>
-        <nav class="nav justify-content-center  ">
+        <nav class="nav" >
             <router-link class="nav-link active" :to="{ 'name': 'home' }" aria-current="page">Home </router-link>
             <router-link class="nav-link " :to="{ 'name': 'about' }" aria-current="page">About </router-link>
             <router-link class="nav-link " :to="{ 'name': 'contacts' }" aria-current="page">Contacts </router-link>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,30 +1,66 @@
 <script>
+import axios from 'axios';
+
 export default {
-    name: 'home'
+    name: 'home',
+    data() {
+        return {
+            base_url: 'http://127.0.0.1:8000/',
+            projects_API: 'api/projects',
+            loading: true,
+            projects: null,
+            error: null
+        }
+    },
+    methods: {
+        getProjects(url) {
+            axios
+                .get(url)
+                .then(response => {
+                    this.projects = response.data.results
+                    this.loading = false
+                    console.log(this.projects);
+                })
+                .catch(error => {
+                    console.log(error);
+                    this.error = error.message
+                })
+        },
+        getImagePath(path) {
+            return path;
+        }
+
+    },
+    mounted() {
+        const url = this.base_url + this.projects_API
+        this.getProjects(url)
+    }
 }
+
 </script>
 <template>
     <div class="container">
         <h1>Home page</h1>
 
-        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-xl-4 g-4">
-            <div class="col" v-for="project in projects.data">
-                <div class="card h-100">
-                    <img class="card-img-top" :src="getImagePath(projects.cover_image)" alt="">
-                    <div class="card-body">
-                        <h3>
-                            {{ project.title }}
-                        </h3>
-                        <p>
-                            {{ truncateText(project.content) }}
-                        </p>
-                        <router-link :to="{ name: 'single-project', params: { 'slug': project.slug } }">Read more</router-link>
-                    </div>
-                    <div class="card-footer">
-                        <span class="badge bg-primary">{{ project.id }}</span>
-                    </div>
+        <div class="row">
+            <div class="col-6  p-2" v-for="project in projects">
 
+                <div class="card mb-3 h-100 p-3 " >
+                    <div class="row g-0">
+                        <div class="col-md-6">
+                            <img :src="getImagePath(project.cover_image)" class="img-fluid rounded-start" alt="{{ project.title }}">
+                        </div>
+                        <div class="col-md-6">
+                            <div class="card-body">
+                                <h5 class="card-title">{{ project.title }}</h5>
+                                <p class="card-text">{{ project.content }}</p>
+                            </div>
+                        </div>
+                        <strong>Type:{{ project.type }}</strong>
+                        
+                    </div>
                 </div>
+
             </div>
         </div>
     </div>
