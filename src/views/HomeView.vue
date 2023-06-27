8a1a6ea1ee65d4ff59f59d3eb6fe6a2504cff8c9
fabiodelli <ntqdel@hotmail.com>
2023-06-27 21:07:38 +0200

    add img modify views

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,68 +1,33 @@
 <script>
-import axios from 'axios';
+
 
 export default {
     name: 'home',
     data() {
         return {
-            base_url: 'http://127.0.0.1:8000/',
-            projects_API: 'api/projects',
-            loading: true,
-            projects: null,
-            error: null,
-            max_text_length : 150, 
-            no_text:'N/A'       
+
         }
     },
     methods: {
-        getProjects(url) {
-            axios
-                .get(url)
-                .then(response => {
-                    this.projects = response.data.results
-                    console.log(this.projects);
-                    this.loading = false
-                })
-                .catch(error => {
-                    console.log(error);
-                    this.error = error.message
-                })
-        },
-        getImagePath(path) {
-            return path;
-        },
-        prevPage(path) {
-            console.log(path);
-            this.getProjects(path);
-        },
-        nextPage(path) {
-            //console.log(path);
-            this.getProjects(path);
-        },
-        truncateText(text) {
-            if (text && text.length > this.max_text_length) {
-                //console.log(text.slice(0, 30));
-                //console.log(text.substring(0, 30));
-                return text.slice(0, this.max_text_length) + '...';
-            }
-            else{
-                return this.no_text
-            }
-            //console.log(text);
-            return text
-        }
-
-    },
-    mounted() {
-        const url = this.base_url + this.projects_API
-        this.getProjects(url)
     }
 }
 
 </script>
 <template>
-    
+        <div class="container d-flex">
+            <div class="col-6 d-flex justify-content-center align-items-center">
+                <h1 class="home_text">HI I'M FABIO <br> JR  WEB DEVELOPER</h1>
+            </div>
+            <div class="col-6 ">
+                <img class="blu" src="../../public/img/blobblu.svg" alt="">
+                <img class="rosa" src="../../public/img/blobrosa.svg" alt="">
+                <img  class="verde" src="../../public/img/blobverde.svg" alt="">
+                <img class="fabio" src="../../public/img/FabioDelli.png" alt="">
+            </div>
+        </div>
 </template>
 
 
-<style lang="scss" scoped></style>
+<style lang="scss">
+@use '../styles/general.scss';
+</style>
