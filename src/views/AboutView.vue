1c346e12efb1c174704ddb2724e42616ce23ee8b
fabiodelli <ntqdel@hotmail.com>
2023-06-27 21:07:16 +0200

    add img modify view

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -4,12 +4,33 @@ export default {
 }
 </script>
 <template>
-    <div class="container">
-        <h1>About page</h1>
-        
+    <div class="container d-flex">
+        <div class="col-6 d-flex justify-content-center align-items-center">
+            <div class="about_text">
+                <h1>ABOUT ME</h1>
+                <strong> Sono un appassionato junior web developer con una solida formazione full stack. Ho frequentato un corso
+                    intensivo a tempo pieno presso Boolean, mirato a sviluppare le competenze necessarie per affrontare il mondo
+                    dello sviluppo web in modo completo. Durante i sei mesi di corso, ho approfondito le mie conoscenze in
+                    JavaScript, Vue JS, PHP, MySQL, Laravel, nonché HTML e CSS.
 
+                    Oltre alla mia formazione tecnica, possiedo un diploma di scuola secondaria di secondo grado presso
+                    l'Istituto Tecnico Agrario Busdraghi.
+
+                    Inoltre, ho partecipato a percorsi formativi focalizzati su mindset e crescita personale, comunicazione
+                    efficace, economia e finanza.
+                </strong>
+            </div>
+        </div>
+        <div class="col-6 ">
+            <img class="blu" src="../../public/img/blobblu.svg" alt="">
+            <img class="rosa" src="../../public/img/blobrosa.svg" alt="">
+            <img  class="verde" src="../../public/img/blobverde.svg" alt="">
+            <img class="fabio" src="../../public/img/FabioDelli.png" alt="">
+        </div>
     </div>
 </template>
 
 
-<style lang="scss" scoped></style>
+<style lang="scss">
+@use '../styles/general.scss';
+</style>
