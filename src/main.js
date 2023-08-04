cdf8c4d88d27df4f0ddb488cbfeec8442e04a305
fabiodelli <ntqdel@hotmail.com>
2023-08-05 00:12:08 +0200

    import fortawesome

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import { router } from './router.js'
+import '@fortawesome/fontawesome-free/css/all.css';
 
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
